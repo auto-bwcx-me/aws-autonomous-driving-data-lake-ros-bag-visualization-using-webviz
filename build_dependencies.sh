30de56241eaed4f34529ad4821219f86e06e3e7a
Robin Long <binlon@amazon.com>
2022-03-21 19:29:30 +0800

    update build

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -2,7 +2,5 @@
 
 mkdir -p webviz_source
 git clone https://github.com/cruise-automation/webviz.git webviz_source/
-cd webviz_source
 # docker build -f webviz_source/Dockerfile-static-webviz webviz_source/
-docker build -f Dockerfile-webviz-nginx . -t webviz-nginx
-cd ..
+# docker build -f Dockerfile-webviz-nginx . -t webviz-nginx
