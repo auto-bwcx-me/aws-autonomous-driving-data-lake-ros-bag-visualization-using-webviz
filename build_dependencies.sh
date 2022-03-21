7ea1ed04fc74a576c243e1d67f47278da36d274d
Robin Long <binlon@amazon.com>
2022-03-21 18:05:23 +0800

    update build scripts

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -2,4 +2,7 @@
 
 mkdir -p webviz_source
 git clone https://github.com/cruise-automation/webviz.git webviz_source/
+npm run bootstrap # install dependencies
+npm run build-static-webviz # generate static build in __static_webviz__
+npm run serve-static-webviz # serve static build on localhost:8080
 docker build -f webviz_source/Dockerfile-static-webviz webviz_source/
